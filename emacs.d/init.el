af8824c128440e907f8bdc89413f6a46a99aa0e6
wanglf <wlfqware@gmail.com>
2016-08-02 21:15:27 +0800

    add mit-scheme support

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -107,6 +107,7 @@
 (require 'init-dash)
 (require 'init-ledger)
 (require 'init-textinfo)
+(require 'init-mitscheme)
 ;; Extra packages which don't require any configuration
 
 (require-package 'gnuplot)
---
+++ emacs.d/lisp/init-mitscheme.el
@@ -0,0 +1,10 @@
+;;; add mit scheme support
+(setq scheme-program-name  "/usr/local/bin/mit-scheme")
+(cond
+ ((string-equal system-type "windows-nt") ; Microsoft Windows
+  (progn
+    (message "Microsoft Windows")))
+ ((string-equal system-type "darwin") ; Mac OS X
+  (setenv "MITSCHEME_LIBRARY_PATH" "/Applications/mit-scheme.app/Contents/Resources"))
+ ((string-equal system-type "gnu/linux") ; linux
+  (setenv "MITSCHEME_LIBRARY_PATH" "/opt/mit-scheme/lib/mit-scheme-x86-64")))
